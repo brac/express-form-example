67801ccccb3b74cd4687af5da89927efb0940466
Ben Bracamonte <ben.bracamonte@gmail.com>
2018-04-11 13:59:11 -0500

    stretch goal

--- app.js
+++ app.js
@@ -5,22 +5,26 @@ const bodyParser = require('body-parser')
 const app = express()
 
 app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
 
 // Using an absolute path is safer
 app.use(express.static(`${__dirname}/public`));
 
-app.get('/form-get', (req, res) => {
-
-  // res.render('index')
-})
-
-app.get('/form-post', (req, res) => {
-
-  // res.render('index')
-})
-
 app.all('/form-submit', (req, res) =>{
-  res.json({message: 'The button is working!'})
+  res.json({
+    queryParams: req.query,
+    bodyParams: req.body
+  })
+})
+
+app.get('/:uri', (req, res) => {
+  if (req.params.uri == 'form-get'){
+    res.render('form-get', {root: __dirname})
+  } else if (req.params.uri == 'form-post'){
+    res.render('form-post', {root: __dirname})
+  } else {
+    res.send('It is form-get or form-post please')
+  }
 })
 
 app.listen(3000, () => {
